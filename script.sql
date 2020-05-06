df8802f48d2ba6fd345049027d0ce32bece0770c
Gaurav Setia <gaurav.singh.setia@gmail.com>
2020-05-06 01:57:30 -0400

    initial git commit

---
+++ script.sql
@@ -0,0 +1,4 @@
+CREATE TABLE countries (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name TEXT, hint TEXT, capital TEXT); 
+INSERT INTO countries (name, hint, capital) values ('united states of america', 'The land of the free', 'Washinton, D.C');
+INSERT INTO countries (name, hint, capital) values ('canada', 'Maple syrup', 'Ottawa');
+INSERT INTO countries (name, hint, capital) values ('china', 'Most populous country', 'Beijing');
